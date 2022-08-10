82844783e5c9d00314d557920fc261b471abb63c
Dr-Arayuki <dr.arayuki@gmail.com>
2022-08-10 16:51:11 +0700

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
-  end
-end
+     @message = Message.new
+     @room = Room.find(params[:room_id])
+   end
+ 
+   def create
+     @room = Room.find(params[:room_id])
+     @message = @room.messages.new(message_params)
+     if @message.save
+       redirect_to room_messages_path(@room)
+     else
+       render :index
+     end
+   end
+ 
+ 
+   private
+ 
+   def message_params
+     params.require(:message).permit(:content).merge(user_id: current_user.id)
+   end
+ end
+ 
