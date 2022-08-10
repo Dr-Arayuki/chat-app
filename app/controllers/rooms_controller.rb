684771264bc1bf56baaa25442034f1b78774e619
Dr-Arayuki <dr.arayuki@gmail.com>
2022-08-10 15:39:55 +0700

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  validates :name, presence: true
+
 end
