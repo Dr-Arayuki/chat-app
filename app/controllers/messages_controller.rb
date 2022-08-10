1ecfd464f90e1fc854c1270ac9da05ac9a090bc8
Dr-Arayuki <dr.arayuki@gmail.com>
2022-08-10 16:53:23 +0700

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
+
   def index
      @message = Message.new
      @room = Room.find(params[:room_id])
+     @messages = @room.messages.includes(:user)
    end
  
    def create
@@ -10,10 +12,10 @@ class MessagesController < ApplicationController
      if @message.save
        redirect_to room_messages_path(@room)
      else
+       @messages = @room.messages.includes(:user)
        render :index
      end
    end
- 
  
    private
  
@@ -21,4 +23,3 @@ class MessagesController < ApplicationController
      params.require(:message).permit(:content).merge(user_id: current_user.id)
    end
  end
- 
