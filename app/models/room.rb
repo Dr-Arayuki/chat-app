8019ea407fc6c7efa981a9523ebd68f56e1ac08f
Dr-Arayuki <dr.arayuki@gmail.com>
2022-08-10 15:49:43 +0700

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+  has_many :messages
 
   validates :name, presence: true
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable,  :validatable
-  validates :name, presence: true
+         :recoverable, :rememberable, :validatable
 
   has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
+
+  validates :name, presence: true
 end
